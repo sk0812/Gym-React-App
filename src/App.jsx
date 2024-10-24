e799666d2771a410892b6f2154ca2add8bc2a466
Siddhanth Kheria <sidkheria@Siddhanths-MacBook-Pro.local>
2024-10-24 21:20:27 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Generator from "./components/Generator";
+import Hero from "./components/Hero";
+import Workout from "./components/Workout";
+
+function App() {
+  return (
+    <main>
+      <Hero/>
+      <Generator/>
+      <Workout/>
+    </main>
+  )
+}
+
+export default App;
